49ce1a179f0b6bfdf250cae1db636496457e6cd3
sainik-khaddar <sainikwarror132@gmail.com>
2022-02-12 02:44:09 +0530

    Live price tracking + UI UX done

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { makeStyles } from "@material-ui/core";
+import Homepage from "./Pages/HomePage";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import CoinPage from "./Pages/CoinPage";
+import Header from "./components/Header";
+
+const useStyles = makeStyles(() => ({
+  App: {
+//backgroundColor: "#14161a",
+  backgroundImage:"url('https://media.istockphoto.com/videos/4k-resolution-christmas-background-defocused-gold-colored-particles-video-id1172143282?s=640x640')",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Route path="/" component={Homepage} exact />
+        <Route path="/coins/:id" component={CoinPage} exact />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
