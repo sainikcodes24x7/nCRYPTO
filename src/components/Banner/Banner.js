53b2c72572360fb87cf145e382e584fdf61c2746
sainik-khaddar <sainikwarror132@gmail.com>
2022-02-12 21:02:52 +0530

    Chart type added

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,63 @@
+import { Container, makeStyles, Typography } from "@material-ui/core";
+import Carousel from "./Carousel";
+
+const useStyles = makeStyles((theme) => ({
+  banner: {
+    backgroundImage: "url('https://media.istockphoto.com/photos/game-background-picture-id1061119906?k=20&m=1061119906&s=170667a&w=0&h=SUfRAHwihaY3xMZFZFYzA0rO4hKvWo4jLOWoXfs414U=')",
+  },
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  carousel: {
+    height: "50%",
+    display: "flex",
+    alignItems: "center",
+  },
+}));
+
+function Banner() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            nCrypto
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+           one stop solution for all your crypto trades
+          </Typography>
+        </div>
+        <Carousel />
+      </Container>
+    </div>
+  );
+}
+
+export default Banner;
